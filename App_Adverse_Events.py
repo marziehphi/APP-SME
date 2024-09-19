cc468a0c4200504bb0f3a08cbd1489716b5aff71
Marzieh Farahani <38806592+marziehphi@users.noreply.github.com>
2024-09-19 09:15:57 +0000

    Update App_Adverse_Events.py

--- App_Adverse_Events.py
+++ App_Adverse_Events.py
@@ -5,6 +5,7 @@ import os
 import datetime
 import gspread
 from google.oauth2.service_account import Credentials
+# libraries 
 
 # Display the initial understanding 
 st.subheader("Initial Understanding:")
